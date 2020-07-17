afdd558a3f84d1e237763ceafd95250dc75e2ba2
GeertvanGeest <geert.vangeest@gmail.com>
2020-07-17 10:05:35 +0200

    key dir structure

--- generate_keys.sh
+++ generate_keys.sh
@@ -6,11 +6,11 @@ while read user
     --key-name key_$user \
     --query 'KeyMaterial' \
     --output text \
-    > key_$user.pem
+    > private_keys/key_$user.pem
 
     chmod 400 key_$user.pem
 
     ssh-keygen \
     -y -f key_$user.pem \
-    > key_$user.pub
+    > public_keys/key_$user.pub
   done < $1
