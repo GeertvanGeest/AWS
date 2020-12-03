d479a1f62cbc7a2308cac61888fdcb3358f001f6
Geest, Geert van <geert.vangeest@sib.swiss>
2020-12-03 11:18:08 +0100

    check for user addition if it already exists

--- scripts/add_users.sh
+++ scripts/add_users.sh
@@ -12,24 +12,30 @@ groupadd $GROUP || true
 
 while read user password
   do
-    adduser \
-    --gecos "" \
-    --disabled-password \
-    $user
+    id -u $user > /dev/null
 
-    adduser \
-    $user \
-    $GROUP \
+    if [ $? == 1 ]
+    then
+      adduser \
+      --gecos "" \
+      --disabled-password \
+      $user
+    else
+      echo "$user already has an account! Password and keys will be overwritten."
+    fi
+      adduser \
+      $user \
+      $GROUP \
 
-    echo $user:$password | chpasswd
+      echo $user:$password | chpasswd
 
-    cd /home/$user
-    mkdir .ssh
-    chmod 700 .ssh
-    touch .ssh/authorized_keys
-    chmod 600 .ssh/authorized_keys
-    cat $PUBLIC_KEY_DIR/key_$user.pub >> .ssh/authorized_keys
-    chown -R $user .ssh
+      cd /home/$user
+      mkdir .ssh
+      chmod 700 .ssh
+      touch .ssh/authorized_keys
+      chmod 600 .ssh/authorized_keys
+      cat $PUBLIC_KEY_DIR/key_$user.pub >> .ssh/authorized_keys
+      chown -R $user .ssh
   done < $USER_PASSWD_LIST
 
 # remove users with userdel -r $user
