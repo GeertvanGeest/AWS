b3ba3b68f2f9b389e759ad82c575aa759f56a534
Geert van Geest <geert.vangeest@gmail.com>
2021-02-18 14:04:50 +0100

    corrected typo

--- scripts/generate_mail.sh
+++ scripts/generate_mail.sh
@@ -4,7 +4,9 @@ USERNAME=$1
 PASSWORD=$2
 IP=$3
 
-echo "Here are your credentials to logon to the AWS cloud server:
+echo "In this e-mail you will find your personal credentials required for your upcoming course. 
+
+We will use these credentials to log in to a remote server. 
 
 username: $USERNAME
 
@@ -14,14 +16,14 @@ your private key is in the attachment.
 
 The IP of the server is: $IP
 
-Save the key in a secure place and change the permissions to 400 (chmod 400 key_$USERNAME.pem).
+Save the key in a secure place and change the permissions to read-only (chmod 400 key_$USERNAME.pem).
+
+The remote server will be available at the start of the course.
 
 This is how you login with ssh (only for macOS/Linux):
 
 ssh -i /path/to/key_$USERNAME.pem $USERNAME@$IP
 
 If you are on Windows, use MobaXterm to login with the private key.
-
-The remote server will be available at the start of the course.
 
 "
